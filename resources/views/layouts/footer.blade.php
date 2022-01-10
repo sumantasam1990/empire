c99507701cae5df39cf97b12dd61958818d12bfe
Sumanta Kundu <sumantasam1990@gmail.com>
2022-01-10 21:21:07 +0530

    from mac

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,83 @@
+
+<section class="container-fluid footer">
+    <footer class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div>
+                    <i class="fab fa-twitter" style="margin-right: 20px;"></i>
+                    <i class="fab fa-instagram" style="margin-right: 20px;"></i>
+                    <i class="fab fa-youtube" style="margin-right: 20px;"></i>
+                    <i class="fab fa-facebook" style="margin-right: 20px;"></i>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <p><a href="">Contact us</a></p>
+                <p><a href="">Cookie Policies</a></p>
+            </div>
+            <div class="col-md-3">
+                <p><a href="">Sitemap</a></p>
+                <p><a href="">FAQ</a></p>
+            </div>
+            <div class="col-md-3">
+                <form action="" method="post">
+                    <div class="form-group mb-2">
+                        <input style="background: #ECE5DB; border: none;" class="form-control" type="email" placeholder="Enter Your Email Address">
+                    </div>
+
+                    <button type="submit" class="btn btn-outline-dark btn-lg emp-btn" style="border: none; width: 100%;">SIGN UP</button>
+                </form>
+
+            </div>
+        </div>
+
+    </footer>
+</section>
+
+<section class="container-fluid" style="background: #000; color: #fff; padding: 6px;">
+    <div class="row text-center">
+        <div class="col-12">
+            <p style="margin-top: 5px;">&copy; 2022 All Rights Reserved.</p>
+        </div>
+    </div>
+</section>
+
+
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+<script src="{{ asset('slick.js') }}" type="text/javascript" charset="utf-8"></script>
+
+<script type="text/javascript">
+    $(document).on('ready', function() {
+        $('.center').slick({
+            dots: false,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            touchMove: true
+        });
+
+        $('.one-slide').slick({
+            dots: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            touchMove: true
+        });
+
+        $('.ecommerce-slider').slick({
+            dots: false,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            touchMove: true
+        });
+
+    });
+</script>
+
+
+
+
+
+</body>
+</html>
---
+++ resources/views/search/index.blade.php
@@ -0,0 +1,130 @@
+@include('layouts.header')
+
+<div class="container hero-section mt-4">
+    <div class="row">
+
+        <div class="col-12">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-8">
+                        <img src="{{ asset('imgs/9.jpeg') }}" style="width: 100%; height: 500px; object-fit: cover; border-radius: 0px !important;" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card-body">
+                            <p class="fs-4">1770</p>
+                            <h5 class="card-title fs-4 display-6 mb-4">George V's 1911 hunting trip in Nepal</h5>
+                            <div class="row mb-3">
+                                <div class="col-6">
+                                    <p><span style="color: red;">Concept artist: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Digital artist: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Voice over: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Podcast: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">VR: </span> Lorem</p>
+                                </div>
+                                <div class="col-6">
+                                    <p style="margin-top: -10px;"><span style="color: red;">Paint artist: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Author: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">People: </span> Lorem</p>
+                                </div>
+                            </div>
+
+                            <hr/>
+                            <p class="card-text">
+                                After attending the Delhi Durbar, George V and Queen Mary travelled via boat and train from Patna and Bankipore to Bhikhna Thori railway station on 16 December. They arrived in Nepal at 10 am on Monday, 18 December...
+                            </p>
+                            <p class="card-text" style="float: right;">
+                                <a class="btn btn-outline-dark emp-btn" href=""><i class="fas fa-book-open"></i> Read</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-12">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-8">
+                        <img src="{{ asset('imgs/10.jpeg') }}" style="width: 100%; height: 500px; object-fit: cover; border-radius: 0px !important;" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card-body">
+                            <p class="fs-4">1770</p>
+                            <h5 class="card-title fs-4 display-6 mb-4">George V's 1911 hunting trip in Nepal</h5>
+                            <div class="row mb-3">
+                                <div class="col-6">
+                                    <p><span style="color: red;">Concept artist: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Digital artist: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Voice over: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Podcast: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">VR: </span> Lorem</p>
+                                </div>
+                                <div class="col-6">
+                                    <p style="margin-top: -10px;"><span style="color: red;">Paint artist: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Author: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">People: </span> Lorem</p>
+                                </div>
+                            </div>
+
+                            <hr/>
+                            <p class="card-text">
+                                After attending the Delhi Durbar, George V and Queen Mary travelled via boat and train from Patna and Bankipore to Bhikhna Thori railway station on 16 December. They arrived in Nepal at 10 am on Monday, 18 December...
+                            </p>
+                            <p class="card-text" style="float: right;">
+                                <a class="btn btn-outline-dark emp-btn" href=""><i class="fas fa-book-open"></i> Read</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-12">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-8">
+                        <img src="{{ asset('imgs/5.jpeg') }}" style="width: 100%; height: 500px; object-fit: cover; border-radius: 0px !important;" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card-body">
+                            <p class="fs-4">1770</p>
+                            <h5 class="card-title fs-4 display-6 mb-4">George V's 1911 hunting trip in Nepal</h5>
+                            <div class="row mb-3">
+                                <div class="col-6">
+                                    <p><span style="color: red;">Concept artist: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Digital artist: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Voice over: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Podcast: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">VR: </span> Lorem</p>
+                                </div>
+                                <div class="col-6">
+                                    <p style="margin-top: -10px;"><span style="color: red;">Paint artist: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">Author: </span> Lorem</p>
+                                    <p style="margin-top: -10px;"><span style="color: red;">People: </span> Lorem</p>
+                                </div>
+                            </div>
+
+                            <hr/>
+                            <p class="card-text">
+                                After attending the Delhi Durbar, George V and Queen Mary travelled via boat and train from Patna and Bankipore to Bhikhna Thori railway station on 16 December. They arrived in Nepal at 10 am on Monday, 18 December...
+                            </p>
+                            <p class="card-text" style="float: right;">
+                                <a class="btn btn-outline-dark emp-btn" href=""><i class="fas fa-book-open"></i> Read</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+</div>
+
+@include('layouts.footer')
